29685cb8ee373cb50f35115d26a76eeb51a53b4d
fauzandifa <fauzankaslana48@gmail.com>
2024-04-01 12:08:14 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="/src/assets/WhatsApp Image 2024-04-01 at 10.42.35.jpeg" width="185" height="185" />
+
+    <div class="wrapper">
+      <HelloWorld msg="hallo semua :)" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,63 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Pendidikan</template>
+
+    saya merupakan seorang mahasiswa disebuah kampus yg bernama 
+    <a href="https://uir.ac.id/" target="_blank" rel="noopener">universitas islam riau</a>
+    sebelumnya saya bersekolah di sman 2 tambang
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>tempat tanggal lahir</template>
+
+    saya lahir di
+    <a href="" target="_blank" rel="noopener">Tembilahan</a>. sebuah kabupaten dengan "seribu parit"
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>jurusan</template>
+
+    saya memilih
+    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">teknik informatika</a>,
+    sebagai jurusan saya
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Game</template>
+
+    saat ini saya memainkan game
+    <a href="https://www.callofduty.com/mobile" target="_blank" rel="noopener">call of duty mobile</a>,
+    <a href="https://www.cookierun-kingdom.com/en/" target="_blank" rel="noopener"
+      >cookie run kingdom</a>,
+    <a href="https://pubg.com/en-na" target="_blank" rel="noopener">pubgm.</a>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>social media</template>
+    <a href="" target="_blank" rel="noopener">IG:Fauzandiffas</a>.
+  </WelcomeItem>
+</template>
